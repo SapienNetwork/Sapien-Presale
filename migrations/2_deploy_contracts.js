f0cb49ea44d53328e9c050a20fd9001177c5e797
Elliot Shohet <elliot.shohet@gmail.com>
2017-09-17 10:44:49 -0700

    Move logic around such that crowdsale needs to be initalized by the owner

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,22 @@
 let SapienCrowdsale = artifacts.require("./SapienCrowdSale.sol");
 let SapienCoin = artifacts.require('./SapienCoin.sol');
 
-module.exports = function(deployer, network, accounts) {
+module.exports = async function(deployer, network, accounts) {
 
     const startBlock = web3.eth.blockNumber + 300;
     const endBlock = startBlock + 300;
     const rate = new web3.BigNumber(1000);
-    const wallet = web3.eth.accounts[0];
+    const wallet = web3.eth.accounts[0]; //TODO: Add multisignature wallet
     const cap = new web3.BigNumber(83000000000000000000000); //83k ether hardcap
-    deployer.deploy(SapienCrowdsale, startBlock, endBlock, rate, wallet, cap);
 
-    //only deploy SapienCoin if we're running tests or are on the development network
-    if(network === 'development')
-        deployer.deploy(SapienCoin);
+    await deployer.deploy(SapienCoin, {from: accounts[0]});
+    await deployer.deploy(SapienCrowdsale, {from: accounts[0]});
+
+    //initalize crowdsale
+    await web3.eth.contract(SapienCrowdsale.abi).at(SapienCrowdsale.address)
+        .initalize(startBlock, endBlock, rate, wallet, cap, SapienCoin.address, {from: accounts[0], gas: 900000});
+
+
 
 };
 
--- test/SapienCrowdsale.js
+++ test/SapienCrowdsale.js
@@ -1,23 +1,33 @@
 let SapienCrowdSale = artifacts.require('./SapienCrowdSale.sol');
+let SapienCoin = artifacts.require('./SapienCoin.sol');
 
 const assertFail = require("./helpers/assertFail");
 
 contract('SapienCrowdSale', function(accounts) {
 
-    const _startBlock = web3.eth.blockNumber + 300;
-    const _endBlock = _startBlock + 300;
-    const _rate = new web3.BigNumber(1000);
-    const _wallet = web3.eth.accounts[0];
-    const _cap = new web3.BigNumber(83000000000000000000000); //83k ether hardcap
+    const startBlock = web3.eth.blockNumber + 300;
+    const endBlock = startBlock + 300;
+    const rate = new web3.BigNumber(1000);
+    const wallet = web3.eth.accounts[0];
+    const cap = new web3.BigNumber(83000000000000000000000); //83k ether hardcap
+
+    let SPN;
+
+    beforeEach(async () => {
+        SPN = await SapienCoin.new();
+    });
 
     it("Deploys contract with correct hardcap", async function() {
-        let crowdsale = await SapienCrowdSale.new(_startBlock, _endBlock, _rate, _wallet, _cap, { from: accounts[0] });
+        let crowdsale = await SapienCrowdSale.new({ from: accounts[0] });
+        crowdsale.initalize(startBlock, endBlock, rate, wallet, cap, SPN.address, {from: accounts[0], gas: 900000});
         let hardcap = await crowdsale.cap.call();
-        assert.equal(hardcap.toString(), _cap.toString(), "Deployed hardcap is not equal to hardcap");
+        assert.equal(hardcap.toString(), cap.toString(), "Deployed hardcap is not equal to hardcap");
     });
 
     it("Checks that nobody can buy before the crowdsale begins", async function() {
-        let crowdsale = await SapienCrowdSale.new(_startBlock, _endBlock, _rate, _wallet, _cap, { from: accounts[0] });
+        let crowdsale = await SapienCrowdSale.new({ from: accounts[0] });
+        crowdsale.initalize(startBlock, endBlock, rate, wallet, cap, SPN.address, {from: accounts[0], gas: 900000});
+
         await assertFail(async function() {
             await crowdsale.buyTokens(accounts[1], { value: web3.toWei(1), from: accounts[1] });
         });
