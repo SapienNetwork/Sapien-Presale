03e3769d211357e146bac8901f235f95ba996b85
Elliot Shohet <elliot.shohet@gmail.com>
2017-09-16 17:37:28 -0700

    Update testrpc settings

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "5",
+      network_id: "*",
       from: "0xB24e066a214736aD653D8AA4a310abEBF7FB23A7",
       gasPrice: "1000000000" //1 Gwei
     }
